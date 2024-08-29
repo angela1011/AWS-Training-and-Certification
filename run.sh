17e043d6f58906857c8ec350f7ff4d1315f2d60d
angela1011 <angela.891011@gmail.com>
2024-08-29 10:21:14 +0800

    update

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ git add .
 git config user.name angela1011
 git config user.email angela.891011@gmail.com
 # git config user.email r12224203@ntu.edu.tw
-git commit -m 'update new course'
+git commit -m 'update'
 git push
